338f0cc561801148fad6f6fd8eea5fe1d753a993
SG1181200783 <surendrenganeshan22@gmail.com>
2025-05-13 05:21:01 -0700

    Update login.php

--- SimpleClinic/login.php
+++ SimpleClinic/login.php
@@ -8,3 +8,13 @@ function log_audit($conn, $username, $role_attempted, $status) {
     $stmt->bind_param("ssss", $username, $role_attempted, $status, $ip);
     $stmt->execute();
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+    $selected_role = $_POST['selected_role'];
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $res = $stmt->get_result();
